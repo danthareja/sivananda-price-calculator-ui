0a3f4c4e982e5eba55b5d5cb421fe37f238e7acd
Dan Thareja <danthareja@gmail.com>
2017-03-28 12:34:23 -0400

    Add test cases for discounts

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,9 +1,5 @@
-// Add tests for:
-// 2. Winter -> Summer transition
-// 3. Discounts
-
 import moment from './lib/moment'
-import { ROOM_ID } from './data/constants'
+import { ROOM_ID, DISCOUNT } from './data/constants'
 import calculator from './calculator'
 
 const winter = moment('2016-11-20').startOf('day').hour(12)
@@ -239,6 +235,29 @@ describe('stays with courses', function() {
     expect(withoutVAT.total).toEqual(2890)
     expect(withVAT.total).toEqual(3106.75)
   })
+
+  // Two best friends decide to ditch Atlantis because they hear about our Thai yoga Massage Workshop...THE Kam Thye Chow?!?
+  // They are sharing a garden room with bath and staying at the ashram for a total of 7 nights.
+  // The workshop is a course that is $295 and 3 days long. No prob ladies!
+  it('two friends staying 7 nights sharing a garden room bath taking a 3 day course for $295 during the winter', function() {
+    const { perGuestWithVAT, withVAT } = calculator({
+      guests: 2,
+      stays: [{
+        roomId: ROOM_ID.GARDEN_BATH,
+        checkInDate: winter.clone(),
+        checkOutDate: winter.clone().add(7, 'days')
+      }],
+      courses: [{
+        tuition: 295,
+        startDate: winter.clone(),
+        endDate: winter.clone().add(3, 'days')
+      }]
+    })
+    // Anne's calculations: $1,391.05 per person or $2,782.18 total
+    expect(perGuestWithVAT.total).toEqual(1391.05)
+    expect(withVAT.total).toEqual(2782.10)
+  })
+
 })
 
 describe('stays with room sharing', function() {
@@ -387,4 +406,124 @@ describe('stays with room moves', function() {
     expect(withoutVAT.total).toEqual(1762)
     expect(withVAT.total).toEqual(1894.15)
   })
+})
+
+describe('stays with discounts', function() {
+  // A TTC graduate stays for 14 nights in a beach hut that they would like to share with another guest (stranger).
+  // This graduate is taking Brahmaswaroop's "Dual path of yoga practice and Code writing" yoga course, which is 295 and 5 days long.
+  // What will their folio balance be?!
+  // Note: TTC graduates get 10% off gross.
+  it('A TTC graduate staying 14 nights sharing a beach hut taking a 4 day course for $295 during the winter', function() {
+    const { withVAT } = calculator({
+      guests: 1,
+      stays: [{
+        roomId: ROOM_ID.BEACH_HUT_SHARING,
+        checkInDate: winter.clone(),
+        checkOutDate: winter.clone().add(14, 'days')
+      }],
+      courses: [{
+        tuition: 295,
+        startDate: winter.clone(),
+        endDate: winter.clone().add(4, 'days')
+      }],
+      discount: {
+        type: DISCOUNT.PERCENT,
+        value: 10
+      }
+    })
+    // Anne's calculation: $2,081.10
+    expect(withVAT.total).toEqual(2081.0925)
+  })
+
+  // A burnt out Wall Street worker has decided to vacay on "da islands".
+  // She chooses to stay in a beachfront deluxe room for 10 nights,
+  // but also signs up for two courses and pays in full upon arrival.
+  // The first course is "Power of Playing Violin like a real Yogi" by beloved Iswara Chaitanya
+  // and the second is "Taking Yoga to Work With You".
+  // Each course is $295 each and the first one is 3 days while the second is 4 days.
+  // Can this economic guru trade her briefcase for some mala beads?
+  // Note: 2 courses signed up for upon arrival and paid are 5% off each.
+  // Note: Assuming at least 1 day between courses
+  it('one person staying in a beachfront deluxe for 10 nights, taking a 3 day course for $295 and a 4 day course for $295 during the winter', function() {
+    const { withVAT } = calculator({
+      guests: 1,
+      stays: [{
+        roomId: ROOM_ID.BEACHFRONT,
+        checkInDate: winter.clone(),
+        checkOutDate: winter.clone().add(10, 'days')
+      }],
+      courses: [{
+        tuition: 295,
+        startDate: winter.clone(),
+        endDate: winter.clone().add(3, 'days'),
+        discount: {
+          type: DISCOUNT.PERCENT,
+          value: 5
+        }
+      }, {
+        tuition: 295,
+        startDate: winter.clone().add(4, 'days'),
+        endDate: winter.clone().add(8, 'days'),
+        discount: {
+          type: DISCOUNT.PERCENT,
+          value: 5
+        }
+      }]
+    })
+    // Anne's calculations: $3,818.94
+    expect(withVAT.total).toEqual(3818.9375)
+  })
+
+  // A young hippy is looking for a chance to learn some new things.
+  // He is staying in his own tent for 25 nights and taking three courses.
+  // All of which he was registered for before he arrived and paid in full upon arrival.
+  // Essentials 1 is 3 days and $195, Essentials 2 is 4 days and $295, then Essentials 3 is $400 and 6 days.
+  // Note: 3 courses signed up for upon arrival and paid for are 10% off each.
+  // Note: Assuming at least 1 day between courses
+  it('one person staying in a tent for 25 nights taking a 3 day course for $195, a 4 day course for $295, and a 6 day course for $400 during the winter', function() {
+    const { withVAT } = calculator({
+      guests: 1,
+      stays: [{
+        roomId: ROOM_ID.TENT_SPACE,
+        checkInDate: winter.clone(),
+        checkOutDate: winter.clone().add(25, 'days')
+      }],
+      courses: [{
+        tuition: 195,
+        startDate: winter.clone(),
+        endDate: winter.clone().add(3, 'days'),
+        discount: {
+          type: DISCOUNT.PERCENT,
+          value: 10
+        }
+      }, {
+        tuition: 295,
+        startDate: winter.clone().add(4, 'days'),
+        endDate: winter.clone().add(8, 'days'),
+        discount: {
+          type: DISCOUNT.PERCENT,
+          value: 10
+        }
+      }, {
+        tuition: 400,
+        startDate: winter.clone().add(9, 'days'),
+        endDate: winter.clone().add(15, 'days'),
+        discount: {
+          type: DISCOUNT.PERCENT,
+          value: 10
+        }
+      }]
+    })
+    // Anne's calculations: $2,729.43
+    expect(withVAT.total).toEqual(2729.425)
+  })
+})
+
+describe('stays with TTC', function() {
+  // A guest arrives on March 23rd and is planning on departing March 30th, staying in a beloved north tent hut.
+  // While at the ashram, for obvious reasons, they fall in love with Sivananda yoga
+  // and feel it is their duty to become a teacher to share this energy with others (WOW 😮)!!
+  // They would like to join the TTC beginning March 31 and ending April 30.
+  // How much will their total be? - Anne's calculation: $4,519.30
+
 })
